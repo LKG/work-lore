62a72ab729b5843911e3377e3ff0d8d5e9c80c5c
gg <lkg61230413@163.com>
2019-04-18 17:34:17 +0800

    上一篇下一篇

--- work-cms/work-cms-domain/src/main/java/im/heart/cms/dto/ArticleDTO.java
+++ work-cms/work-cms-domain/src/main/java/im/heart/cms/dto/ArticleDTO.java
@@ -1,12 +1,26 @@
 package im.heart.cms.dto;
 
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
 
+import javax.persistence.Transient;
 import java.math.BigInteger;
 @Data
 public class ArticleDTO {
-    BigInteger id;
-    String title;
-    String author;
+    private BigInteger id;
+    private String title;
+    private String author;
+    private String source;
+    private String type;
+    private String summary;
+    private Boolean allowComment=Boolean.FALSE;
+    private BigInteger rateTimes=BigInteger.ZERO;
     Long hits;
+    @Transient
+    public String getShortTitle() {
+        if (!StringUtils.isBlank(title) && (title.length() > 14)) {
+            return title.substring(0, 14) + "...";
+        }
+        return title;
+    }
 }
