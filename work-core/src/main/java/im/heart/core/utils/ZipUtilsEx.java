94e9afda39cf5734de39a0486fbb49d15f690193
gg <lkg61230413@163.com>
2019-06-25 14:44:48 +0800

    增加压缩方法

--- work-core/src/main/java/im/heart/core/utils/ZipUtilsEx.java
+++ work-core/src/main/java/im/heart/core/utils/ZipUtilsEx.java
@@ -5,20 +5,100 @@ import org.apache.commons.compress.archivers.zip.ZipArchiveEntry;
 import org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream;
 import org.apache.commons.compress.archivers.zip.ZipUtil;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.zip.CRC32;
+import java.util.zip.CheckedOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipUtilsEx extends ZipUtil {
 	protected static final Logger logger = LoggerFactory.getLogger(ZipUtilsEx.class);
+	private static final int BUFFER = 1024 * 8;
+
 	public static void zip(String zipFilePath,File ... files) {
 		File zipFile = new File(zipFilePath);
 		zip(zipFile,files);
 	}
+	/**
+	 * 压缩文件或目录
+	 * @param fromPath 待压缩文件或路径
+	 * @param toPath   压缩文件，如 xx.zip
+	 */
+	public static void compress(String fromPath, String toPath) throws IOException {
+		File fromFile = new File(fromPath);
+		File toFile = new File(toPath);
+		if (!fromFile.exists()) {
+			throw new FileNotFoundException(fromPath + "不存在！");
+		}
+		try (
+				FileOutputStream outputStream = new FileOutputStream(toFile);
+				CheckedOutputStream checkedOutputStream = new CheckedOutputStream(outputStream, new CRC32());
+				ZipOutputStream zipOutputStream = new ZipOutputStream(checkedOutputStream)
+		) {
+			compress(fromFile, zipOutputStream, "");
+		}
+	}
+
+	/**
+	 * 压缩文加
+	 * @param file
+	 * @param zipOut
+	 * @param baseDir
+	 * @throws IOException
+	 */
+	private static void compress(File file, ZipOutputStream zipOut, String baseDir) throws IOException {
+		if (file.isDirectory()) {
+			compressDirectory(file, zipOut, baseDir);
+		} else {
+			compressFile(file, zipOut, baseDir);
+		}
+	}
+
+	/**
+	 * 压缩目录
+	 * @param dir
+	 * @param zipOut
+	 * @param baseDir
+	 * @throws IOException
+	 */
+	private static void compressDirectory(File dir, ZipOutputStream zipOut, String baseDir) throws IOException {
+		File[] files = dir.listFiles();
+		if (files != null && ArrayUtils.isNotEmpty(files)) {
+			for (File file : files) {
+				compress(file, zipOut, baseDir + dir.getName() + "/");
+			}
+		}
+	}
+
+	/**
+	 * 文件压缩
+	 * @param file
+	 * @param zipOut
+	 * @param baseDir
+	 * @throws IOException
+	 */
+	private static void compressFile(File file, ZipOutputStream zipOut, String baseDir) throws IOException {
+		if (!file.exists()) {
+			return;
+		}
+		try (BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file))) {
+			ZipEntry entry = new ZipEntry(baseDir + file.getName());
+			zipOut.putNextEntry(entry);
+			int count;
+			byte[] data = new byte[BUFFER];
+			while ((count = bis.read(data, 0, BUFFER)) != -1) {
+				zipOut.write(data, 0, count);
+			}
+		}
+	}
+
+
+
+
 	public static void zip(File zipFile,File ... files) {
 		ZipArchiveOutputStream zaos = null;
 		try {
@@ -33,7 +113,7 @@ public class ZipUtilsEx extends ZipUtil {
 				InputStream is = null;
 				try{
 					is = new FileInputStream(file);
-					byte[] buffer = new byte[1024 * 5]; 
+					byte[] buffer = new byte[BUFFER];
 					int len = -1;
 					while((len = is.read(buffer)) != -1) {
 						zaos.write(buffer, 0, len);
