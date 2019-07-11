98d4bb661fc1d1c4dd231261b67d8aa2cfb9848d
lkg61230413@163.com <lkg61230413@163.com>
2019-07-12 00:48:46 +0800

    移除editord.md

--- work-admin/src/main/resources/static/js/admin/cms/article_create.js
+++ work-admin/src/main/resources/static/js/admin/cms/article_create.js
@@ -7,52 +7,11 @@ define(function(require, exports, moudles) {
 		api : baseRoot + "/admin/article",
 		createApi:baseRoot + "/admin/article/save.json",
 	};
-	var editormd = require('editor.md');
 	var $httpUtil = require('/js/common/httpUtil.js');
 	var dialog = require('/js/common/dialog');
 	 require('/js/common/validate');
 	var $msg= require('/js/common/alerts.js');
 	window.dialog = dialog;
-	require('link-dialog');
-	require('reference-link-dialog');
-	require('code-block-dialog');
-	require('image-dialog');
-	require('table-dialog');
-	require('emoji-dialog');
-	require('goto-line-dialog');
-	require('html-entities-dialog');
-	require('help-dialog');
-	require('preformatted-text-dialog');
-	var testEditor=editormd("test-editormd", {
-        height: 460,
-        path : '/modules/editor.md/lib/',
-        //toolbar  : false,             // 关闭工具栏
-        codeFold : true,
-        searchReplace : true,
-        saveHTMLToTextarea : true,      // 保存 HTML 到 Textarea
-        htmlDecode : "style,script,iframe|on*",            // 开启 HTML 标签解析，为了安全性，默认不开启
-        emoji : true,
-        taskList : true,
-        tocm            : true,          // Using [TOCM]
-        tex : true,                      // 开启科学公式 TeX 语言支持，默认关闭
-        atLink: true, 
-        flowChart : false,                // 疑似 Sea.js与 Raphael.js 有冲突，必须先加载 Raphael.js ，Editor.md 才能在 Sea.js 下正常进行；
-        //dialogMaskOpacity : 0.4,    // 设置透明遮罩层的透明度，全局通用，默认值为 0.1
-        //dialogMaskBgColor : "#000", // 设置透明遮罩层的背景颜色，全局通用，默认为 #fff
-        saveHTMLToTextarea : true,
-        imageUpload : true,
-        imageUploadURL : "./admin/upload.php",
-        onload : function() {
-      //      console.log('onload', this);
-//            this.fullscreen();
-//            this.unwatch();
-//            this.watch().fullscreen();
-// 			  this.setMarkdown("#PHP"+this.settings.pluginPath); 
-//            this.width("100%");
-//            this.height(480);
-//            this.resize("100%", 640);
-        }
-    });	
 	 var $mainForm=$("#J_articleForm");
 	 $mainForm.validate({
 			rules: {
