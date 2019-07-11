34107a48c428a15e37364ccead00c2200bf1278b
gg <lkg61230413@163.com>
2019-07-11 15:20:44 +0800

    移除无效代码

--- work-web/src/main/resources/static/js/cms/article-details.js
+++ work-web/src/main/resources/static/js/cms/article-details.js
@@ -4,13 +4,16 @@ define(function (require, exports, moudles) {
     var template = require('arttemplate');
     var dialog = require('artDialog');
     var $baseRoot=$("#baseRoot");
-    var laypage = require('laypage');
-    var printThis = require('printThis');
+    var $httpUtil = require('/js/common/httpUtil.js');
+    var $docSupportContainer=$("#doc-support-container");
+    require('printThis');
+    alert(jQuery);
+    debugger;
     window.dialog = dialog;
     var baseRoot=$baseRoot.attr("href");
-    var $httpUtil = require('/js/common/httpUtil.js');
+
     //TODO：后期考虑公用全局配置
-    var $docSupportContainer=$("#doc-support-container");
+
     var url = {
         api : baseRoot + "/article",
     };
@@ -18,7 +21,7 @@ define(function (require, exports, moudles) {
         $("#top-banner").hide();
     });
     $("#btn-print").on("click", function() {
-        $("#article-row").printThis({ importCSS: true,});
+        $("#article-row").printThis();
     });
     $(".artinfo .cgray").on("click", function() {
         var font=$(this).attr("data-font");
